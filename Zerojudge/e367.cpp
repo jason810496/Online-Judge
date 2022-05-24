0a2960b79a055d264477d48b08e79bbd61235293
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-05-24 10:47:19 +0800

    AC range XOR

---
+++ Zerojudge/e367.cpp
@@ -0,0 +1,62 @@
+/*
+ ██████╗ ██████╗ ██████╗ ███████╗     ██████╗ ██╗   ██╗████████╗
+██╔════╝██╔═══██╗██╔══██╗██╔════╝    ██╔═══██╗██║   ██║╚══██╔══╝
+██║     ██║   ██║██║  ██║█████╗      ██║   ██║██║   ██║   ██║   
+██║     ██║   ██║██║  ██║██╔══╝      ██║   ██║██║   ██║   ██║   
+╚██████╗╚██████╔╝██████╔╝███████╗    ╚██████╔╝╚██████╔╝   ██║   
+ ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝     ╚═════╝  ╚═════╝    ╚═╝   
+                                                                
+ █████╗  ██████╗    ██╗███╗   ██╗                               
+██╔══██╗██╔════╝    ██║████╗  ██║                               
+███████║██║         ██║██╔██╗ ██║                               
+██╔══██║██║         ██║██║╚██╗██║                               
+██║  ██║╚██████╗    ██║██║ ╚████║                               
+╚═╝  ╚═╝ ╚═════╝    ╚═╝╚═╝  ╚═══╝                                                                                                                                                                            
+*/
+#pragma GCC optimize("O2")
+#include<bits/stdc++.h>
+using namespace std;
+#define OAO cin.tie(0);ios_base::sync_with_stdio(0);
+#define F first
+#define S second
+#define ll long long 
+#define range(x) x.begin(),x.end()
+#define MEM(x,i) memset(x,i,sizeof(x))
+#define rep(i,n) for(int i=0;i<n;++i)
+#define REP(i,st,n) for(int i=st;i<=n;++i)
+#define PB push_back
+typedef pair<int,int> pii;
+const int MAX_N =1e5+5;
+const int N = 2e6+5;
+const int INF = 1e9;
+
+/*
+    AC 
+    https://zerojudge.tw/ShowProblem?problemid=e367
+*/
+
+int Prefix[MAX_N];
+
+void Build(){
+    Prefix[0]=0;
+
+    int count = 0 ;
+    for(int i=1;i<MAX_N;i++){
+
+        count^=i;
+
+        Prefix[i]=Prefix[i-1]^count;
+    }
+}
+
+signed main(){
+    OAO
+    Build();
+
+    int L ,R;
+    while(cin>>L>>R){
+        cout<< (Prefix[R]^Prefix[L-1]) <<'\n';
+    }
+
+    return 0;
+}
