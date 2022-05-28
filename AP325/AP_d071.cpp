9141d49b1332402d8cfcb7e4998da7b6ed46e928
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-05-28 16:27:07 +0800

    fix bug : init array

--- AP325/AP_d071.cpp
+++ AP325/AP_d071.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main(){
     cin.tie(0);ios_base::sync_with_stdio(0);
     int n,Weight,MAX_w=0,w[MAX_N],val[MAX_N]; 
-    ll dp[MAX_W];
+    ll dp[MAX_W] = {};
     cin>>n>>Weight;
     
     for(int i=0;i<n;i++)cin>>w[i];
