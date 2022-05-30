c2e9331ce19e71d7a7e3ae6df4931166588fada4
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-05-30 11:24:39 +0800

    Add reference

---
+++ AP325/AP_d081.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define OAO cin.tie(0);ios_base::sync_with_stdio(0);
+#define N (1<<20)
+
+int DP[N] , Wt[N] , n ;
+
+int dp(int cur){
+    if( DP[ cur ] >=0 ) return DP[cur];
+
+    int MX_From=0;
+
+    for(int i=0;i<n;i++){
+        // one bit diff 
+        if( cur&(1<<i) ){
+            // back to last state
+            MX_From = max( MX_From , dp( cur^(1<<i) ) );
+        }
+    }
+
+    return DP[ cur ] = MX_From + Wt[ cur ];
+}
+
+signed main(){
+    OAO 
+    memset(DP , -1 , sizeof(DP) );
+
+    cin>>n;
+    for(int i=0;i<(1<<n) ; i++){
+        cin>>Wt[i];
+    }
+
+    DP[0]=Wt[0];
+
+    cout<<dp( (1<<n)-1 );
+    return 0;
+}
