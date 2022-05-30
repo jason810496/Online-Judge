f0c8e6bb7f7a80e70987ac338a9dad0337c15aad
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-05-31 07:18:56 +0800

    Fix bugs & AC

--- AP325/AP_d094.cpp
+++ AP325/AP_d094.cpp
@@ -3,38 +3,38 @@ using namespace std;
 int main(){
     cin.tie(0);ios_base::sync_with_stdio(0);
     int n , target ,L , R;
-    int S[100005],dis[100005];
     
     cin>>n>>target>>L>>R;
+
+    vector<int> dis(n,-1) , S(n);
+    
     for(int i=0;i<n;i++) cin>>S[i];
 
-    memset(dis,-1,sizeof(dis));
     queue<int> q;
-    
-    int index,iL,iR;
+
     q.push(0);
     dis[0]=0;
-    while(q.size() && dis[target]==-1){
-        index=q.front();
+    
+
+    while( q.size() && dis[ target]==-1){
+        int cur = q.front();
         q.pop();
-        iL=index+L;
-        iR=index+R;
-        if(iL>0 && S[iL]>0 && S[iL]<n &&S[iL]==-1){
-            q.push(S[iL]);
-            dis[S[iL]]=dis[index]+1;
+
+        int idx = cur-L;
+
+        if( idx>=0 && S[ idx ]>=0 && S[idx]<n && dis[ S[idx] ]==-1 ){
+            q.push( S[idx] );
+            dis[ S[idx] ] = dis[ cur ]+1;
         }
-      
-            
-        if(iR<n && S[iR]>0 && S[iR]<n && S[iR]==-1){
-            q.push(S[iR]);
-            dis[S[iR]]=dis[index]+1;
-            
+        idx = cur+R;
+
+        if( idx<n && S[ idx ]>=0 && S[idx]<n && dis[ S[idx] ]==-1 ){
+            q.push( S[idx] );
+            dis[ S[idx] ] = dis[ cur ]+1;
         }
-         
+
     }
-    // for(int i=0;i<n;i++){
-    //     cout<<dis[i]<<' ';
-    // }
+
     cout<<dis[target];
     return 0;
 }
