88cee54a1b133b1d40b47f01aa9560f92b4b2bb9
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-05-30 17:54:31 +0800

    Fix bugs

--- AP325/AP_d093.cpp
+++ AP325/AP_d093.cpp
@@ -2,13 +2,18 @@
 using namespace std;
 #define F first 
 #define S second
-// BUG ??
+
+// AC BFS
+
+int n,m,Graph[505][505],oper[505][505];
+int pos[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
+
+inline int in(int x,int y){
+    return !(x<=0 || y<=0 || x>n || y>m);
+}
+
 int main(){
     cin.tie(0);ios_base::sync_with_stdio(0);
-
-    int n,m,Graph[505][505];
-    int oper[505][505];
-    int pos[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
 
     char c;
     cin>>n>>m;
@@ -28,69 +33,27 @@ int main(){
         oper[1][1]=4;
     }
     
-    while(q.size() ){
-        auto p=q.front();
+
+    while( q.size() ){
+        int x=q.front().F ; 
+        int y=q.front().S ;
         q.pop();
 
-        int I__,J__;
-        //0:R , 1:D ,2:L ,3:U
-        for(int i=0;i<4;i++){
-            I__=p.F+pos[i][0];
-            J__=p.S+pos[i][1];
+        for(int k=0;k<4;k++){
+            int i=x+pos[k][0] , j=y+pos[k][1];
 
-            if(I__>n||J__>m||I__<=0||J__<=0) continue;
+            while( in(i,j) && Graph[i][j]!=-1 ){
+                if( Graph[i][j]==0){
+                    Graph[i][j]= Graph[x][y]+( oper[x][y]==4 ? 0:(oper[x][y]==k ? 0:1));
+                    oper[i][j]=k;
+                    q.push( {i,j} );
+                }
 
-            if(Graph[I__][J__]==-1 ) continue;
-
-            // visited[I__][J__]=1;
-
-            int next= Graph[p.F][p.S] + (oper[p.F][p.S]==4 ? 0:(oper[p.F][p.S]==i ? 0:1) ) ;
-            // Graph[I__][J__]= ( Graph[I__][J__]==0? next: min(Graph[I__][J__],next) );
-
-            if( Graph[I__][J__]==0 ){
-                oper[I__][J__]=i;
-                Graph[I__][J__] = next;
-                q.push( {I__,J__} );
+                i+=pos[k][0] , j+=pos[k][1];
             }
-            
         }
     }
-    // for(int i=1;i<=n;i++){
-    //     for(int j=1;j<=m;j++){
-    //         cout<<oper[i][j]<<' ';
-    //     }
-    //     cout<<'\n';
-    // }
-    // for(int i=1;i<=n;i++){
-    //     for(int j=1;j<=m;j++){
-    //         cout<<Graph[i][j]<<' ';
-    //     }
-    //     cout<<'\n';
-    // }
 
     cout<<(Graph[n][m]>0 ? Graph[n][m]-1:-1);
     return 0;   
 }
-/*
-
-
-5 5
-00000
-01110
-00100
-10010
-00000
-
-10 10 
-0000000000
-0111111110
-0111000110
-0011010110
-1000010110
-1111110110
-0000000110
-0111111110
-0111111110
-0000000000
-
-*/
