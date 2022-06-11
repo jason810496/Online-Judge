fbc315517bfd7c004aad6c233cd47f22d33aaf13
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-06-11 16:43:16 +0800

    top down 3D DP

--- Leetcode/2022_5_8/4.cpp
+++ Leetcode/2022_5_8/4.cpp
@@ -11,61 +11,41 @@ using namespace std;
 #define REP(i,st,n) for(int i=st;i<=n;++i)
 #define PB push_back
 typedef pair<int,int> pii;
-const int MAX_N = 100005;
+const int N = 105;
 const int INF = 1e9;
-
-struct Node{
-    int L,R,x,y;
-    Node(int l=0,int r=0,int X=0,int Y=0){
-        L=l;
-        R=r;
-        x=X;
-        y=Y;
-    }
-};
 
 class Solution {
 public:
-    int MX_step ;
-    int half;
-    int n , m;
-    int pos[2][2] = {{0,1},{1,0}};
-    bool flag = false;
+    int n , m , mx , total ;
+    int dp[N][N][N] ;
+    bool solve(vector< vector<char> > &g , int i,int j ,int cnt){
+        if( i>=n ||j>=m ) return 0;
 
+        if( g[i][j] =='(') cnt++;
+        else cnt--;
+
+        if( cnt<0 || cnt>mx){
+            return 0;
+        }
+
+        if( i==n-1 && j==m-1 ){
+            return cnt==0;
+        }
+
+        if( dp[i][j][cnt] !=-1 ) return dp[i][j][cnt];
+
+        return dp[i][j][cnt] = ( solve(g,i+1,j,cnt) | solve(g,i,j+1,cnt) );
+        
+    }
     bool hasValidPath(vector<vector<char>>& grid) {
         n=grid.size();
         m=grid[0].size();
+        total = n+m-1;
+        if( total&1 || grid[0][0]==')' || grid[n-1][m-1]=='(' ) return false;
+        mx = total /2 ;
 
-        MX_step = n+m-1;
-        if( MX_step%2 ==1 ) return false;
-        half = MX_step/2;
-
-        queue< Node> q;
-
-        q.push( Node(0,0,0,0) );
-
-        while(q.size() ) {
-            Node P = q.front()  ; q.pop();
-            if( grid[P.x][P.y]=='(') P.L++;
-            else P.R++;
-
-            // cout<<P.x<<' '<<P.y<' '<<grid[P.x][P.y]<<'\n';
-
-            if( P.x==n-1 && P.y==m-1){
-                if( P.L==P.R ) return true;
-            }
-
-            if( P.L > half || P.R > half || P.R>P.L ) continue;
-
-            for(int k=0;k<2;k++){
-                int i  = P.x+pos[k][0] , j = P.y+pos[k][1];
-                if( i>=n || j>=m ) continue;
-
-                q.push( Node(i,j,P.L,P.R) );
-            }
-        }
-
-        return false;
+        memset( dp , -1 ,sizeof(dp) );
+        return solve(grid,0,0,0);
     }
 };
 
